f68c76307bc6d3352106b7b8274fa136b5e8616d
ProphetNoha <prophet.noha@proton.me>
2023-06-28 00:17:11 +0330

    add some atrribute to emploee

--- user/data.go
+++ user/data.go
@@ -1,7 +1,28 @@
 package user
 
+import "github.com/google/uuid"
+
 type UserData struct {
 	name     string
-	id       int
+	id       uuid.UUID
 	birthday string
+}
+
+func (u *UserData) SetName(name string) {
+	u.name = name
+}
+func (u *UserData) Setid() {
+	u.id = uuid.New()
+}
+func (u *UserData) Setbirthday(b string) {
+	u.birthday = b
+}
+func (u *UserData) GetName() string {
+	return u.name
+}
+func (u *UserData) Getid() uuid.UUID {
+	return u.id
+}
+func (u *UserData) Getbirthday() string {
+	return u.birthday
 }
--- user/method.go
+++ user/method.go
@@ -1,1 +1,10 @@
 package user
+
+type UserUsecase interface {
+	setName(name string)
+	setbirthday(birthday string)
+	setid()
+	getName() string
+	getbirthday() string
+	getid() int
+}
